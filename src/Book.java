e8cf4944679715e898e28333296a69737d597ae3
Khaled Alokby <kalokby@ecitele.com>
2020-02-20 21:45:56 +0200

    adding whight functions

--- src/Book.java
+++ src/Book.java
@@ -1,4 +1,4 @@
-public class Book {
+public class Book implements Comparable{
     int id;
     int score;
 
@@ -8,5 +8,13 @@ public class Book {
             return false;
         Book other = (Book) obj;
         return id == other.id;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        if (o == null || !(o instanceof Book))
+            System.out.println("Fuck u");
+        Book other = (Book)o;
+        return score-other.score;
     }
 }
--- src/Library.java
+++ src/Library.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Library {
@@ -7,4 +8,22 @@ public class Library {
     int signUpProcess;
     int booksPerDay;
     List<Book> books = new ArrayList<>();
+    public int workingDays(){
+        return GoogleBooks.daysToEnd-signUpProcess;
+    }
+    public int maxScore(){
+        books.sort(new Comparator<Book>() {
+            @Override
+            public int compare(Book o1, Book o2) {
+                return o1.score-o2.score;
+            }
+        });
+        int result = 0;
+        int booksMax = booksPerDay*workingDays();
+        for (int i = 0; i < booksMax; i++) {
+             result += books.get(i).score;
+        }
+        return result;
+    }
+
 }
